a0cee2d5d0bd1251039d4dab1cb749ae08dac9d5
maramabaidi <maram.abaidi@etudiant-enit.utm.tn>
2024-03-24 02:48:48 +0100

    jkdhs

--- src/components/ContactForm/Contact.js
+++ src/components/ContactForm/Contact.js
@@ -42,24 +42,16 @@ function Contact() {
   };
 
   return (
-    <div>
-      <div className="wrapper">
-        <div className="title">
-          <h1>CONTACTEZ-NOUS</h1>
-        </div>
-        <div className="contact-form" >
-          <form className="input-fields" onSubmit={handleSubmit}>
-            <div><input type="text" className="input" name="name" value={formData.name} onChange={handleChange} placeholder="Nom" />
-            <input type="email" className="input" name="email" value={formData.email} onChange={handleChange} placeholder="Adresse Email" />
-            <input type="tel" className="input" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} placeholder="Téléphone" />
-            <input type="text" className="input" name="subject" value={formData.subject} onChange={handleChange} placeholder="Sujet" /></div>
-            <div className="msg">
-              <textarea className="input" name="messageContent" value={formData.messageContent} onChange={handleChange} placeholder="Message"></textarea>
-              <button type="submit" className="btn">Envoyer</button>
-            </div>
-          </form>
-        </div>
-      </div>
+    <div className="form-wrapper">
+      <h1 className="form-title">Contactez-nous</h1>
+      <form className="form-fieldset" onSubmit={handleSubmit}>
+        <input type="text" className="input-field" name="name" value={formData.name} onChange={handleChange} placeholder="Nom" />
+        <input type="email" className="input-field" name="email" value={formData.email} onChange={handleChange} placeholder="Adresse Email" />
+        <input type="tel" className="input-field" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} placeholder="Téléphone" />
+        <input type="text" className="input-field" name="subject" value={formData.subject} onChange={handleChange} placeholder="Sujet" />
+        <textarea className="textarea-field" name="messageContent" value={formData.messageContent} onChange={handleChange} placeholder="Message"></textarea>
+        <button type="submit" className="btn-submit">Envoyer</button>
+      </form>
     </div>
   );
 }
